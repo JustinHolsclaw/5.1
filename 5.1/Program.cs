3f68aad3602c95b5016a846054547c913923cc48
Justin Holsclaw <Justin.Holsclaw@snow.edu>
2022-04-30 23:36:16 -0600

    initial commit

---
+++ 5.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int remotePort = 6544;
+IPEndPoint server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), remotePort);
+
+while (true)
+{
+    string input = Console.ReadLine();
+    Dictionary<string, string> prevRequests = new Dictionary<string, string>();
+    if (prevRequests.ContainsKey(input))
+    {
+        Console.WriteLine($"Previously Found {input} => {prevRequests[input]}");
+    }
+    else
+    {
+        Task.Run(() =>
+        {
+            UdpClient client = new UdpClient();
+            byte[] data = Encoding.UTF8.GetBytes(input);
+            client.Send(data, data.Length, server);
+
+            IPEndPoint remote = new IPEndPoint(0, 0);
+            byte[] response = client.Receive(ref remote);
+            client.Close();
+
+            string answer = Encoding.UTF8.GetString(response);
+            Console.WriteLine($"Original {input} => {answer}");
+            prevRequests.Add("12", "15");
+            Console.WriteLine(prevRequests.Count());
+        });
+    }
+}
